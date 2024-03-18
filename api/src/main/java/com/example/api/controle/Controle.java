d8ffbc96be1b186b944cb0861de0b5873b534c48
Josias <josiassalermojunior>
2024-03-18 05:10:57 -0300

    aula 05 Implementando controle

---
+++ api/src/main/java/com/example/api/controle/Controle.java
@@ -0,0 +1,14 @@
+package com.example.api.controle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controle {
+
+  @GetMapping("")
+  public String mensagem() {
+    return "Hello World!";
+  }
+
+}
