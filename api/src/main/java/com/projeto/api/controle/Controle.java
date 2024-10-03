1df0ed4561b12bc40998abe5588d9ef645296f00
Josias <josiassalermojunior>
2024-10-03 10:31:58 -0300

    Aula 06 Utilizando @PathVariable

--- api/src/main/java/com/projeto/api/controle/Controle.java
+++ api/src/main/java/com/projeto/api/controle/Controle.java
@@ -1,6 +1,7 @@
 package com.projeto.api.controle;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,15 @@ public class Controle {
   public String mensagem() {
     return "Hello World";
   }
+
+  @GetMapping("/boasVindas")
+  public String boasVindas() {
+    return "Seja bem vindo(a)";
+  }
+
+  @GetMapping("/boasVindas/{nome}")
+  public String boasVindas(@PathVariable String nome) {
+    return "Seja bem vindo(a)" + nome;
+  }
+
 }
